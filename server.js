bf640a87120074527825d224a14093ccd915c360
zoya9545-web <zoya9545@gmail.com>
2026-04-21 08:57:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "html")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "html/index.html"));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Running on port ${port}`);
 });
